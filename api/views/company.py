d49934ed3b5eb560f4d581400b1f291c0d871348
Deodato Junior <deodatojunior999@gmail.com>
2022-06-07 23:48:50 -0400

    initiate auth

--- api/views/company.py
+++ api/views/company.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
 from api.models.company.models import Company
 from api.serializers.company import *
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
